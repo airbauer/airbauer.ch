ec78ef200cb7e92dbf213d9ad061aee302bdf90a
AIRbauer <airbauer@proton.me>
2024-07-21 03:41:19 +0200

    upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import NavHost from '../components/molecules/NavHost.svelte';
+	import Hero from '../components/organisms/Hero.svelte';
+	import About from '../components/organisms/About.svelte';
+	import Repos from '../components/organisms/Repos.svelte';
+	import Supporters from '../components/organisms/Supporters.svelte';
+	import Footer from '../components/molecules/Footer.svelte';
+</script>
+
+<NavHost />
+<main>
+	<Hero />
+	<About />
+	<Repos />
+	<Supporters />
+	<Footer />
+</main>
