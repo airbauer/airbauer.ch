7dbb22f87c5d90270bc40d655b228c169ce52338
Vik <airbauer@proton.me>
2024-09-08 17:36:07 +0200

    plswork

--- src/components/molecules/NavHost.svelte
+++ src/components/molecules/NavHost.svelte
@@ -30,7 +30,7 @@
 					/></svg
 				>
 			</Nav>
-			<Nav href="#work" section="work" isSelected={ y > 675 && y < 1000}>
+			<Nav href="#work" section="work" isSelected={y > 675 && y < 1000}>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -38,7 +38,7 @@
 					fill="var(--accent)"
 					viewBox="0 0 24 24"
 					><path fill="none" d="M0 0h24v24H0z" /><path
-						d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
+						d="M14 5v14h-4V5h4m2-2h-6v18h6V3zm-2 6h-2v4h2v-4z"
 					/></svg
 				>
 			</Nav>
