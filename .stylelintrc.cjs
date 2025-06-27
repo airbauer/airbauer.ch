12f616c8d4962fbd0de23e0ff65b518d5cc542bb
Vik <airbauer@proton.me>
2025-06-27 03:40:07 +0200

    fixfix

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ["stylelint-config-standard-scss"],
-  plugins: ["stylelint-prettier"],
   rules: {
-    "prettier/prettier": true,
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
     "selector-class-pattern": null,
--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -1,28 +1,32 @@
-export { matchers } from './matchers.js';
+export { matchers } from "./matchers.js";
 
 export const nodes = [
-	() => import('./nodes/0'),
-	() => import('./nodes/1'),
-	() => import('./nodes/2')
+  () => import("./nodes/0"),
+  () => import("./nodes/1"),
+  () => import("./nodes/2"),
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2]
-	};
-
-export const hooks = {
-	handleError: (({ error }) => { console.error(error) }),
-	
-	reroute: (() => {}),
-	transport: {}
+  "/": [2],
 };
 
-export const decoders = Object.fromEntries(Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]));
+export const hooks = {
+  handleError: ({ error }) => {
+    console.error(error);
+  },
+
+  reroute: () => {},
+  transport: {},
+};
+
+export const decoders = Object.fromEntries(
+  Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]),
+);
 
 export const hash = false;
 
 export const decode = (type, value) => decoders[type](value);
 
-export { default as root } from '../root.svelte';
+export { default as root } from "../root.svelte";
