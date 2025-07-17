5557a850a9b97e3d5b7bf7d478ca8f1a0537075e
Vik <airbauer@proton.me>
2025-07-17 12:51:28 +0200

    $$$

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    visualizer({ open: true, filename: "bundle-report.html" }),
+  ],
 };
 
 export default config;
